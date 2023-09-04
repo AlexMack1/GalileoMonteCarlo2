21cd0c91ead1c444b4f6c1d4243a8941ea07c42f
Marcos Simon <67840553+AlexMack1@users.noreply.github.com>
2023-09-03 22:02:25 -0600

    app.py

--- app.py
+++ app.py
@@ -18,13 +18,12 @@ y = data['Sales']
 model = LinearRegression().fit(X, y)
 
 # Función para mostrar el gráfico de dispersión
-def plot_regression(data, model):
+def plot_regression(data):
     fig, ax = plt.subplots(1, 3, figsize=(20, 6))
     features = ['TV', 'Radio', 'Newspaper']
 
     for i, feature in enumerate(features):
         ax[i].scatter(data[feature], y, alpha=0.5)
-        ax[i].plot(data[feature], model.predict(data), color='red')
         ax[i].set_title(f'{feature} vs. Sales')
         ax[i].set_xlabel(feature)
         ax[i].set_ylabel('Sales')
@@ -61,13 +60,14 @@ st.title('Simulación de Montecarlo para Inversión Publicitaria')
 # Número de simulaciones
 num_simulations = st.number_input('Número de simulaciones', min_value=100, value=1000)
 
-# Mostrar el gráfico de regresión
-st.write('Gráfico de regresión lineal:')
-st.pyplot(plot_regression(X, model))
-
 # Botón para ejecutar simulación
 if st.button('Ejecutar simulación'):
     best_budgets, max_sales = monte_carlo_simulation(num_simulations)
+    
+    # Mostrar el gráfico de regresión
+    st.write('Gráfico de regresión lineal:')
+    st.pyplot(plot_regression(X))
+
     st.write(f'Mejor presupuesto para TV: ${best_budgets[0]:.2f}')
     st.write(f'Mejor presupuesto para Radio: ${best_budgets[1]:.2f}')
     st.write(f'Mejor presupuesto para Periódico: ${best_budgets[2]:.2f}')
