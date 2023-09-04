0d73cf9b9c34a84c36abac12047b262c3ddeb88c
Marcos Simon <67840553+AlexMack1@users.noreply.github.com>
2023-09-03 21:53:35 -0600

    app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from scipy.stats import triang
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Carga de datos
 @st.cache
@@ -16,32 +17,29 @@ X = data[['TV', 'Radio', 'Newspaper']]
 y = data['Sales']
 model = LinearRegression().fit(X, y)
 
-# Función de simulación de Montecarlo
-def monte_carlo_simulation(n=1000):
-    tv_min, tv_max = X['TV'].min(), X['TV'].max()
-    radio_min, radio_max = X['Radio'].min(), X['Radio'].max()
-    newspaper_min, newspaper_max = X['Newspaper'].min(), X['Newspaper'].max()
+# Función para mostrar el gráfico de dispersión
+def plot_regression(data, model):
+    fig, ax = plt.subplots(1, 3, figsize=(20, 6))
+    features = ['TV', 'Radio', 'Newspaper']
 
-    tv_random = triang.rvs((X['TV'].median() - tv_min) / (tv_max - tv_min), loc=tv_min, scale=tv_max - tv_min, size=n)
-    radio_random = triang.rvs((X['Radio'].median() - radio_min) / (radio_max - radio_min), loc=radio_min, scale=radio_max - radio_min, size=n)
-    newspaper_random = triang.rvs((X['Newspaper'].median() - newspaper_min) / (newspaper_max - newspaper_min), loc=newspaper_min, scale=newspaper_max - newspaper_min, size=n)
-    
-    max_sales = -np.inf
-    best_budgets = None
-    
-    for tv, radio, newspaper in zip(tv_random, radio_random, newspaper_random):
-        sales_pred = model.predict([[tv, radio, newspaper]])[0]
-        if sales_pred > max_sales:
-            max_sales = sales_pred
-            best_budgets = tv, radio, newspaper
-            
-    return best_budgets, max_sales
+    for i, feature in enumerate(features):
+        ax[i].scatter(data[feature], y, alpha=0.5)
+        ax[i].plot(data[feature], model.predict(data), color='red')
+        ax[i].set_title(f'{feature} vs. Sales')
+        ax[i].set_xlabel(feature)
+        ax[i].set_ylabel('Sales')
+
+    return fig
 
 # Interfaz Streamlit
 st.title('Simulación de Montecarlo para Inversión Publicitaria')
 
 # Número de simulaciones
 num_simulations = st.number_input('Número de simulaciones', min_value=100, value=1000)
+
+# Mostrar el gráfico de regresión
+st.write('Gráfico de regresión lineal:')
+st.pyplot(plot_regression(X, model))
 
 # Botón para ejecutar simulación
 if st.button('Ejecutar simulación'):
